76345a3dd3c5727184107acc072350a5cf654ebe
Jesse Lloyd <jesse.lloyd@students.fhu.edu>
2017-09-27 17:55:10 -0500

    Adds modestbranding to youtube live stream link.

--- wp-content/plugins/sermon-manager/inc/youtube-live-stream.php
+++ wp-content/plugins/sermon-manager/inc/youtube-live-stream.php
@@ -3,5 +3,5 @@
 add_action('youtube_live_stream', 'get_youtube_live_stream');
 
 function get_youtube_live_stream() {
-    echo '<div class="video-container"><iframe class="video" src="https://www.youtube.com/embed/live_stream?channel=UCH_sJHkYxpuLcetdFD3Vvbg" autoplay=1" frameborder="0" allowfullscreen></iframe></div>';
+    echo '<div class="video-container"><iframe class="video" src="https://www.youtube.com/embed/live_stream?channel=UCH_sJHkYxpuLcetdFD3Vvbg&modestbranding=1&color=red" autoplay=1" frameborder="0" allowfullscreen></iframe></div>';
 }
